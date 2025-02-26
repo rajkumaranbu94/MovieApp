537ad74f9f8a21ba49ca6ef562ed186694dd0ee9
Rajkumar <raj15294@gmail.com>
2025-02-27 02:47:15 +0800

    Code analysis done

--- app/src/main/java/com/fudu/movieapp/MainActivity.kt
+++ app/src/main/java/com/fudu/movieapp/MainActivity.kt
@@ -2,11 +2,8 @@ package com.fudu.movieapp
 
 import MovieSearchAdapter
 import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
--- app/src/main/java/com/fudu/movieapp/data/remote/ApiService.kt
+++ app/src/main/java/com/fudu/movieapp/data/remote/ApiService.kt
@@ -2,7 +2,6 @@ package com.fudu.movieapp.data.remote
 
 import com.fudu.movieapp.data.response.MovieResponse
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
